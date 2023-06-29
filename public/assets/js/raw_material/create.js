215eb376fa943191353452e7c7ebaba882878ec2
juan049 <juanjrodriguez049@gmail.com>
2023-06-29 05:48:56 -0600

    2023-06-29

--- public/assets/js/raw_material/create.js
+++ public/assets/js/raw_material/create.js
@@ -1,41 +1,49 @@
 document.addEventListener("DOMContentLoaded", (event) => {
-       /*
+    /*
     Componentes----------------------------------------------------------
      */
     //Defino la tabla y los arr
-    const componentsTable = document.querySelector('#components-table');
-    let deleteButtons = componentsTable.querySelectorAll('.btnDeleteRow');
-    let newRowButtons = componentsTable.querySelectorAll('.btnNewRow');
+    const componentsTable = document.querySelector("#components-table");
+    let deleteButtons = componentsTable.querySelectorAll(".btnDeleteRow");
+    let newRowButtons = componentsTable.querySelectorAll(".btnNewRow");
 
-   
-    //A cada  boton le hago foreach y pongo un event listener
-    deleteButtons.forEach(button=>{
-        button.addEventListener('click', (e)=>{
+    //Defino el array de los inputs
+    const rowInputsArr = {};
+
+    //Funciónn para que el boton borre la fila
+    function addDeleteRowFunction(button) {
+        button.addEventListener("click", (e) => {
             //Defino el body de la table
-            const tBody = button.closest('tbody');
+            const tBody = button.closest("tbody");
             //Defino un arrat de tr
-            const trArray = tBody.querySelectorAll('tr');
+            const trArray = tBody.querySelectorAll("tr");
             //Defino el tr padre
-            const parentTr = e.target.closest('tr');
+            const parentTr = e.target.closest("tr");
 
             //Elimino el TR solo si el trArray es mayor a 1
-            if(trArray.length > 1){
-            parentTr.remove();  
+            if (trArray.length > 1) {
+                parentTr.remove();
             }
         });
+    }
+
+    //A cada  boton le hago foreach y pongo un event listener
+    deleteButtons.forEach((button) => {
+        addDeleteRowFunction(button);
     });
 
-    newRowButtons.forEach(button => {
-        button.addEventListener('click', (e)=>{
+    //Función para agregar nueva fila
+    function addAddRowFunction(button) {
+        button.addEventListener("click", (e) => {
             //Defino el body de la table
-            const tBody = button.closest('tbody');
+            const tBody = button.closest("tbody");
             //Defino un arrat de tr
-            const trArray = tBody.querySelectorAll('tr');
+            const trArray = tBody.querySelectorAll("tr");
             //Defino el tr padre
-            const parentTr = e.target.closest('tr');
+            const parentTr = e.target.closest("tr");
 
             //Creo la fila
-            let newTr = document.createElement('tr');
+            let newTr = document.createElement("tr");
             //Defino el html de la fila
             const rowInnerHtml = `
             <td>
@@ -43,6 +51,7 @@ document.addEventListener("DOMContentLoaded", (event) => {
                     <i class="bi bi-plus-lg"></i>
                 </button>
             </td>
+
             <td scope="col"><input type="text" class="inputComponentName"></td>
             <td scope="col"><input type="number" min=0 max=100 class="inputComponentPercentage"></td>
             <td scope="col"><input type="text" class="inputComponentCAS"></td>
@@ -52,16 +61,22 @@ document.addEventListener("DOMContentLoaded", (event) => {
                 </button>
             </td>`;
 
-            newTr.innerHTML=rowInnerHtml;
+            newTr.innerHTML = rowInnerHtml;
+            //Añado la función de borrado
+            addDeleteRowFunction(newTr.querySelector(".btnDeleteRow"));
+            //Añado la función de agregar
+            addAddRowFunction(newTr.querySelector(".btnNewRow"));
 
-          //inserto
-          parentTr.insertBefore(newTr, tr.nextSibling);
+            //Añado
 
+            //inserto despues de donde se dió añadir
+            tBody.insertBefore(newTr, parentTr.nextSibling);
         });
-    })
+    }
 
-
-
+    newRowButtons.forEach((button) => {
+        addAddRowFunction(button);
+    });
 
     /*
     Areas--------------------------------------------------------------
--- resources/views/raw_material/create.blade.php
+++ resources/views/raw_material/create.blade.php
@@ -1,24 +1,24 @@
 @extends('layouts.app')
 
 @section('content')
-<style>
-    table td {
-  position: relative;
-}
+    <style>
+        table td {
+            position: relative;
+        }
 
-table td input {
-  position: absolute;
-  display: block;
-  top:0;
-  left:0;
-  margin: 0;
-  height: 100%;
-  width: 100%;
-  border: none;
-  padding: 10px;
-  box-sizing: border-box;
-}
-</style>
+        table td input {
+            position: absolute;
+            display: block;
+            top: 0;
+            left: 0;
+            margin: 0;
+            height: 100%;
+            width: 100%;
+            border: none;
+            padding: 10px;
+            box-sizing: border-box;
+        }
+    </style>
     <div class="container">
         <h1>Dar de alta un nuevo insumo</h1>
         <form action="{{ route('raw_material.store') }}" method="POST">
@@ -42,47 +42,34 @@ table td input {
                     <h5 class="card-title">Componentes</h5>
                     <input type="hidden" name="components" id="components">
                     <table class="table table-bordered text-center" id="components-table">
-  <thead>
-    <tr>
-      <th scope="col"></th>
-      <th scope="col">Nombre</th>
-      <th scope="col">Porcentaje</th>
-      <th scope="col">Numero CAS</th>
-      <th scope="col"></th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      
-      <td><button type="button" class="btn btn-success btnNewRow"><i class="bi bi-plus-lg"></i></td>
-      <td scope="col"><input type="text" class="inputComponentName"></td>
-      <td scope="col"><input type="number" min=0 max=100 class="inputComponentPercentage"></td>
-      <td scope="col"><input type="text" class="inputComponentCAS"></td>
-      <td>
-            <button type="button" class="btn btn-danger btnDeleteRow">
-                <i class="bi bi-x-lg fw-bold"></i>
-            </button>
-        </td>
-    </tr>
-    <tr>
-      
-        <td>
-            <button type="button" class="btn btn-success btnNewRow">
-                <i class="bi bi-plus-lg"></i>
-            </button>
-        </td>
-        <td scope="col"><input type="text" class="inputComponentName"></td>
-        <td scope="col"><input type="number" min=0 max=100 class="inputComponentPercentage"></td>
-        <td scope="col"><input type="text" class="inputComponentCAS"></td>
-        <td>
-            <button type="button" class="btn btn-danger btnDeleteRow">
-                <i class="bi bi-x-lg fw-bold"></i>
-            </button>
-        </td>
-      </tr>
+                        <thead>
+                            <tr>
+                                <th scope="col"></th>
+                                <th scope="col">Nombre</th>
+                                <th scope="col">Porcentaje</th>
+                                <th scope="col">Numero CAS</th>
+                                <th scope="col"></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
 
-  </tbody>
-</table>
+                                <td><button type="button" class="btn btn-success btnNewRow"><i class="bi bi-plus-lg"></i>
+                                </td>
+                                <td scope="col"><input type="text" class="inputComponentName"></td>
+                                <td scope="col"><input type="number" min=0 max=100 class="inputComponentPercentage">
+                                </td>
+                                <td scope="col"><input type="text" class="inputComponentCAS"></td>
+                                <td>
+                                    <button type="button" class="btn btn-danger btnDeleteRow">
+                                        <i class="bi bi-x-lg fw-bold"></i>
+                                    </button>
+                                </td>
+                            </tr>
+
+
+                        </tbody>
+                    </table>
 
                 </div>
             </div>
